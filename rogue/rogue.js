0b93de1074c2248b5a9832c4d6441c2374db8fac
Shaun Dunne <shaun@studiozero.co>
2016-12-01 23:35:15 +0000

    moving stuff to CDN

--- rogue/rogue.js
+++ rogue/rogue.js
@@ -7,9 +7,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var trailer = document.getElementById('trailer');
   var play = document.getElementById('play-button');
   var scene = document.getElementById('main');
-  var choiceBox = document.getElementById('choose')
+  var choiceBox = document.getElementById('choose');
+  var bgAudio = document.getElementById('bgAudio');
 
   trailer.pause();
+
+  document.querySelector('a-assets').addEventListener('loaded', function() { console.log('assets loaded') });
 
 
   mono.addEventListener("click", function(){
@@ -22,13 +25,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
   })
 
   var enter = function(){
+    bgAudio.pause();
 		trailer.play();
     trailer.pause();
 
     scene.setAttribute('visible', true);
 		scene.style.display = "block";
-
-
   }
 
 
